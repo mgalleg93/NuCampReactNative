f8f03006e4833a8294ae6b6831a78a974590d2f7
Miguel Gallegos <miguelgallegosgt@gmail.com>
2022-05-21 10:54:48 -0700

    Workshop 2

--- redux/comments.js
+++ redux/comments.js
@@ -8,7 +8,24 @@ export const comments = (state = { errMess: null, comments: []}, action) => {
         case ActionTypes.COMMENTS_FAILED:
             return {...state, errMess: action.payload};
 
+        case ActionTypes.ADD_COMMENT:
+            const newComment = {
+                id: state.comments.length,
+                ...action.payload
+            }
+            return {...state, comments: state.comments.concat(newComment) };
+
         default:
             return state;
     }
 };
+
+
+/*[, {
+                id: comments.length, 
+                campsiteId: action.payload.campsiteId,
+                rating: action.payload.rating,
+                text: action.payload.text,
+                author: action.payload.author,
+                date: action.payload.date
+            }]*/
