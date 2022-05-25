769be7505b7112de8e56e03fe91c9c4ad472fefb
Miguel Gallegos <miguelgallegosgt@gmail.com>
2022-05-24 17:08:04 -0700

    05242022 commit 2

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
 import { ScrollView, Text } from 'react-native';
 import { Card } from 'react-native-elements';
+import * as Animatable from 'react-native-animatable';
 
 function ContactCard () {
     return(
-        <Card
-            title='Contact Information'
-            wrapperStyle={{margin: 20}}>
-            <Text>1 NuCamp Way</Text>
-            <Text>Seattle, WA 98001</Text>
-            <Text style={{marginBottom: 10}}>U.S.A.</Text>
-            <Text>Phone: 1-206-555-1234</Text>
-            <Text>Email: campsites@nucamp.co</Text>
-        </Card>
+        <ScrollView>
+            <Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
+                <Card
+                    title='Contact Information'
+                    wrapperStyle={{margin: 20}}>
+                    <Text>1 NuCamp Way</Text>
+                    <Text>Seattle, WA 98001</Text>
+                    <Text style={{marginBottom: 10}}>U.S.A.</Text>
+                    <Text>Phone: 1-206-555-1234</Text>
+                    <Text>Email: campsites@nucamp.co</Text>
+                </Card>
+            </Animatable.View>
+        </ScrollView>
     );
 }
 
