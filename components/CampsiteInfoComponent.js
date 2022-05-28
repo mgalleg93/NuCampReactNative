26b32b882e4de880b3cc39a3fc614b76e3c17aae
Miguel Gallegos <miguelgallegosgt@gmail.com>
2022-05-28 08:30:56 -0700

    React Native workshop 3

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -26,6 +26,8 @@ function RenderCampsite(props) {
 
     const view = React.createRef();
 
+    const recognizeComment = ({dx}) => (dx > 200) ? true : false;
+
     const recognizeDrag = ({dx}) => (dx < -200) ? true : false;
 
     const panResponder = PanResponder.create({
@@ -54,6 +56,8 @@ function RenderCampsite(props) {
                     ],
                     { cancelable: false }
                 );
+            } else if (recognizeComment(gestureState)) {
+                props.onShowModal();
             }
             return true;
         }
