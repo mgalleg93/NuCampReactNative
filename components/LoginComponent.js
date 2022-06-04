3e2b667500787550cb1694c10e6afd64922ea0a7
Miguel Gallegos <miguelgallegosgt@gmail.com>
2022-06-04 09:16:14 -0700

    Workshop 4 bonus

--- components/LoginComponent.js
+++ components/LoginComponent.js
@@ -8,6 +8,7 @@ import { createBottomTabNavigator } from 'react-navigation-tabs';
 import { baseUrl } from '../shared/baseUrl';
 import * as ImageManipulator from 'expo-image-manipulator';
 import { SaveFormat } from 'expo-image-manipulator';
+import * as MediaLibrary from 'expo-media-library';
 
 
 class LoginTab extends Component {
@@ -186,6 +187,7 @@ class RegisterTab extends Component {
             {format: SaveFormat.PNG}
         );
         console.log(processedImage);
+        MediaLibrary.saveToLibraryAsync(processedImage.uri);
         this.setState({imageUrl: processedImage.uri});
     }
 
